62053c4340b5701b1204f7cbccaa15b3ad4ec57b
Erik Billing <erik@ekhagen.org>
2022-01-25 20:57:32 +0100

    Some cleanup.

--- src/LCTest.java
+++ src/LCTest.java
@@ -4,16 +4,11 @@
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintStream;
 import java.nio.CharBuffer;
-import java.nio.charset.Charset;
-import java.nio.charset.CharsetDecoder;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.FileSystem;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
